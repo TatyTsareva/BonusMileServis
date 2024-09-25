e112e586cba1ce896859b70f72189b32163a8281
Taty ts <tatytsareva@mail.ru>
2024-09-25 14:46:15 +0300

    first commit

---
+++ src/BonusMilesService.java
@@ -0,0 +1,17 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        int result;
+        int miles = 20;
+        if (price < 1100) {
+            result = price / miles;
+
+        } else
+            result = price / miles;
+
+        return result;
+    }
+}
+
+
+
+
