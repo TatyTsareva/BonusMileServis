f8502493bf680d17c75a575d20f6322eae3ba735
Taty ts <tatytsareva@mail.ru>
2024-09-26 13:59:24 +0300

    updated BonusMilesService

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,16 +2,11 @@ public class BonusMilesService {
     public int calculate(int price) {
         int result;
         int miles = 20;
-        if (price > 0) {
-            result = (price / miles);
+        result = (price / miles);
 
-
-        } else
-            result = price / miles;
         System.out.println("Начислено бонусов");
 
         return result;
-
 
     }
 }
